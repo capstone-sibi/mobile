a69690cdd7e1cb6adcf71127193dfedc77d1b8fe
hcarissa <13520164@std.stei.itb.ac.id>
2023-12-19 13:02:14 +0700

    fix main activity and add quiz fragment

--- Capstone/app/src/main/java/com/example/capstone/MainActivity.kt
+++ Capstone/app/src/main/java/com/example/capstone/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.capstone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.capstone.databinding.ActivityMain2Binding
+import com.example.capstone.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navController: NavController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        NavigationUI.setupWithNavController(navView, navController)
     }
 }
---
+++ Capstone/app/src/main/java/com/example/capstone/ui/quiz/QuizListViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.capstone.ui.quiz
+
+import androidx.lifecycle.ViewModel
+
+class QuizListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
