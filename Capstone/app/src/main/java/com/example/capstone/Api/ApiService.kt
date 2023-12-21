5d1fb8b74b74a8a8acb86cb46354abe494181c6d
Zeus-s <kristabdixx@gmail.com>
2023-12-21 23:28:37 +0700

    remove unnecessary service

--- Capstone/app/src/main/java/com/example/capstone/Api/ApiService.kt
+++ Capstone/app/src/main/java/com/example/capstone/Api/ApiService.kt
@@ -2,20 +2,10 @@ package com.example.capstone.Api
 
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Multipart
-import retrofit2.http.POST
-import retrofit2.http.Part
 
 interface ApiService {
 
     @GET("dictionary")
     fun getDictionary(
-    ) : Call<DictionaryApiResponse>
-
-    @POST("dictionary")
-    @Multipart
-    fun postDictionary(
-        @Part("path") path : String,
-        @Part("meaning") meaning : String
     ) : Call<DictionaryApiResponse>
 }
